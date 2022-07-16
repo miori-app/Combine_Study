75a04e4abd844a3388f52f9b1c6ccf045c021c88
Mi Yeon Lee <immioriii@gmail.com>
2022-07-16 23:39:13 +0900

    220716 passthrough subject 학습

--- Combine Study.playground/Pages/Subject.xcplaygroundpage/Contents.swift
+++ Combine Study.playground/Pages/Subject.xcplaygroundpage/Contents.swift
@@ -3,7 +3,13 @@ import Combine
 
 // PassthroughSubject
 
-
+// output, error type 설정
+let relay = PassthroughSubject<String, Never>()
+let subscription1 = relay.sink { value in
+    print("subscription1 received value : \(value)")
+}
+relay.send("Hi 👋")
+relay.send("Bye 👋")
 
 
 // CurrentValueSubject
