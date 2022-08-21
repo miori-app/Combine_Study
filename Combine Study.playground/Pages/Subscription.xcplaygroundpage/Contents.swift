e44af3aaceed7f335a1097e72beb1fc911f7c6a4
Mi Yeon Lee <immioriii@gmail.com>
2022-08-21 23:45:21 +0900

    PassthroughSubject 공부

--- Combine Study.playground/Pages/Subscription.xcplaygroundpage/Contents.swift
+++ Combine Study.playground/Pages/Subscription.xcplaygroundpage/Contents.swift
@@ -4,9 +4,19 @@ import Foundation
 import Combine
 
 
+let subject = PassthroughSubject<String, Never>()
 
 
 // The print() operator prints you all lifecycle events
+let subscription = subject.sink { value in
+    print("Subscriber received value: \(value)")
+}
 
+subject.send("Hello")
+subject.send("Hello again!")
+subject.send("Hello final")
+
+subject.send(completion: .finished)
+subject.send("gogo ??")
 
 //: [Next](@next)
