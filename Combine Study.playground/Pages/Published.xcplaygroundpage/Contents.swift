d5efb964d582d697676aa0778cb9007271f1a153
Mi Yeon Lee <immioriii@gmail.com>
2022-08-22 23:58:17 +0900

    @Published 학습

--- Combine Study.playground/Pages/Published.xcplaygroundpage/Contents.swift
+++ Combine Study.playground/Pages/Published.xcplaygroundpage/Contents.swift
@@ -5,6 +5,20 @@ import UIKit
 import Combine
 
 
+final class SomeViewModel {
+    @Published var name: String = "miori"
+    var age: Int = 26
+}
 
+final class Label {
+    var text: String = ""
+}
 
+let label = Label()
+let viewModel = SomeViewModel()
+print("text: \(label.text)")
+
+//퍼블리셔
+viewModel.$name.assign(to: \.text, on: label)
+print("text: \(label.text)")
 //: [Next](@next)
