570c26d9c9365c22127b1849405d7aa948803508
Mi Yeon Lee <immioriii@gmail.com>
2022-08-25 01:50:45 +0900

    thread,스케줄러 관리

--- Combine Study.playground/Pages/Scheduler.xcplaygroundpage/Contents.swift
+++ Combine Study.playground/Pages/Scheduler.xcplaygroundpage/Contents.swift
@@ -3,8 +3,21 @@
 import Foundation
 import Combine
 
+let arrPublisher = [1,2,3].publisher
+let queue = DispatchQueue(label: "custom")
 
-
+let subscription = arrPublisher
+    .subscribe(on: queue)
+    .map { value -> Int in
+        print("transform :\(value), thread: \(Thread.current)")
+        return value
+    }
+    .receive(on: DispatchQueue.main)
+    .sink { value in
+        print("received value: \(value), thread: \(Thread.current)")
+    }
 
 
 //: [Next](@next)
+// 비동기에서 중요한점 : thread 잘 고려
+// thread safe
